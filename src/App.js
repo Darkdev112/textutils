7008103456d50f1800ce1bc392ff813833607634
Darkdev112 <devashritcode@gmail.com>
2022-06-29 12:56:21 +0530

    dev

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Alert from './components/Alert';
-import About from './components/About';
+// import About from './components/About';
 
 function App() {
   const [mode,setMode] = useState("light");
@@ -34,16 +34,18 @@ function App() {
     }
   };
   return( 
-    <BrowserRouter>
+    <>
+     {/* <BrowserRouter> */}
       <Navbar title="TextUtils" aboutUs="About TextUtils"  mode={mode} toggleMode={toggleMode} ></Navbar>
       <div className="something" style={{height : "50px", width : "100vw"}}>
         <Alert alert = {alert}></Alert>
       </div>
-      <Routes>
-        <Route exact path="about" element={<About mode= {mode}></About>} />
-        <Route exact path="/" element={<TextForm heading="Enter the text to analyse" mode={mode} showAlert={showAlert}></TextForm>} />
-      </Routes>     
-    </BrowserRouter>
+      {/* <Routes> */}
+         {/* <Route exact path="about" element={<About mode= {mode}></About>} /> */}
+        {/*<Route exact path="/" element={*/}<TextForm heading="Enter the text to analyse" mode={mode} showAlert={showAlert}></TextForm>{/*} />*/}
+      {/* // </Routes>      */}
+    {/* // </BrowserRouter> */}
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import Proptypes from 'prop-types'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Navbar</Link>
+                {/* <Link className="navbar-brand" to="/">Navbar</Link> */}
+                <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">{props.title}</Link>
+                            {/* <Link className="nav-link active" aria-current="page" to="/">{props.title}</Link> */}
+                            <a className="nav-link active" aria-current="page" href="#">{props.title}</a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="about">{props.aboutUs}</Link>
+                            {/* <Link className="nav-link active" aria-current="page" to="about">{props.aboutUs}</Link> */}
+                            <a className="nav-link active" aria-current="page" href="#">{props.aboutUs}</a>
                         </li>
                     </ul>
                     {/* <form className="d-flex" role="search">
